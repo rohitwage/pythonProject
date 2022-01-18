85bd6de3a4cdc55bbb3c9848855b8e99354aefd2
E773527 <rohitwage@tietoevry.com>
2022-01-18 20:29:24 +0530

    changes number of records to only last one test status only

--- excelDataEmailreport.py
+++ excelDataEmailreport.py
@@ -9,28 +9,33 @@ FStatus = FinalStatus.iloc[0]['TestCase']
 
 df = pd.read_csv('C:/Jmeter/apache-jmeter-5.4.1/apache-jmeter-5.4.1/bin/demo1.csv')
 
-print(df.tail(5))
-new_df = df.tail(22)
-print(df.count())
 
+
+print(df.tail(5))
+new_df = df.tail(11)
+print(df.count())
+html = new_df.to_html()
+print(html)
 body = """\
 <html>
   <head> <style>
   .p1 {
   font-family: "sans-serif";
     font-size: 15px;
-    }</style>
-    <style> 
-    br, table, th, td {{font-size:10pt; border:1px solid black; border-collapse:collapse; text-align:left;}}
-    th, td {{padding: 5px;}}
-        </style></head>
+    }
+    table {background-color: #dee1ff;
+    width: 100%; font-size:11pt; border-style:groove; white; border-collapse:collapse; text-align:left;  
+    }
+    tr:nth-child(even) {background-color: #f2f2f2;}
+    </style>
+    </head>
   <body>
 
     <p class="p1">Hi!<br>
     <br></br>
-       Please find the attached report of Jenkins job - Discovery ServiceNow - Jmeter validation and summary as below..<br>
+       Please find the attached report of Jenkins job - Discovery ServiceNow - Jmeter validation and summary as below..
        <br>Latest top 10 status:
-       """ + new_df.to_html(index=False, col_space=150, justify='right',classes='format.css') + """
+       <br>""" + html + """</br>
        <font color ="blue">
        <br> Thanks! </br>
        <br>Note: This is an auto generated email please do not reply</br>
